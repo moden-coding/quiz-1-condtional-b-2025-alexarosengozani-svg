1e9491bbc91fe79a1a09148f35836e2fdc6aa022
alexarosengozani-svg <alexarosengozani@gmail.com>
2025-09-10 09:08:36 -0400

    done

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
- /**
+/**
  *  Basic task (up to B+ level):
  *    - Ask the user for a whole number x.
  *    - Print whether x is EVEN or ODD.
@@ -22,6 +22,37 @@ import java.util.*;
 public class App {
     public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
+        //this is the basic task:
 
-    }
+        System.out.println("Give me a whole number: ");
+        int num = Integer.valueOf(scanner.nextLine());
+        if ((num % 2) == 0) {
+            System.out.println("Number " + num + " is even.");
+        }else if ((num % 2) != 0) {
+            System.out.println("Number " + num + " is odd");
+        }else if (num == 0)
+            System.out.println("ZERO");
+
+        //This is the moderate task:
+
+        System.out.println("Give me another whole number: ");
+        double total = Double.valueOf(scanner.nextLine());
+        if (total<= 0) {
+            System.out.println("ERROR");
+        }else{
+            double calculation = num / total;
+            System.out.println(num + "/" + total + " is " + calculation);
+        }
+        
+        // This is the challenge task:
+        if (num < 0 || num > 100) {
+            System.out.println("Wrong number!");
+        }else if (num == 2 || num == 3 || num == 5 || num == 7) {
+            System.out.println(num + " is a prime number.");
+        }else if (num % 2 == 0 || num % 3 == 0 || num % 5 ==  0 || num % 7 == 0) {
+            System.out.println(num + " is not a prime number.");
+        }else {
+            System.out.println(num + " is a prime number.");
+        }
+    }   
 }
